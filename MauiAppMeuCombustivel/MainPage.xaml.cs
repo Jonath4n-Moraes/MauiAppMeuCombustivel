ee3bcc324402a0aa51471306bea5409d6c58c548
jonat <jonat@DESKTOP-8C3CAH6>
2024-10-11 22:54:21 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiAppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                if(etanol <= (gasolina * 0.7) )
+                {
+                    msg = "O etanol está compensando.";
+                } else
+                {
+                    msg = "A gasolina está compensado.";
+                }
+
+                DisplayAlert("Calculado", msg, "OK");
+
+            } catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+
+
+        } // Fecha Método
+    }//Fecha class
+}//Fecha namespace
